9f55d31625ef89d6f01e36173329ba434e71aa6f
cedani <cedani2akak@gmail.com>
2020-05-24 00:12:48 +0200

    fixing (master): removing password in database  On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   API/database/db.js

--- API/database/db.js
+++ API/database/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 
 const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'DaruixMont2406/'
+    host: 'yourhost',
+    user: 'youruser',
+    password: 'yourpassword'
 })
 
 con.connect((err) => {
